e1a05db9c1382c17601cf3a8a3b3e6ff93cbdbb9
Richard Leiva <richard.leiva@verndale.com>
2020-02-29 12:21:58 -0500

    Add accounts project with the registration logic

---
+++ src/Feature/Feature.Hackathon.Accounts/Controllers/RegistrationController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Feature.Hackathon.Accounts.Models;
+
+namespace Feature.Hackathon.Accounts.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View("/Views/Accounts/Registration.cshtml");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(RegistrationForm formData)
+        {
+
+            return Redirect("/");
+        }
+    }
+}
---
+++ src/Feature/Feature.Hackathon.Accounts/Models/RegistrationForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Feature.Hackathon.Accounts.Models
+{
+    public class RegistrationForm
+    {
+        public string Name1 { get; set; }
+        public string Email1 { get; set; }
+        public string LinkedIn1 { get; set; }
+        public string Twitter1 { get; set; }
+        public string Name2 { get; set; }
+        public string Email2 { get; set; }
+        public string LinkedIn2 { get; set; }
+        public string Twitter2 { get; set; }
+        public string Name3 { get; set; }
+        public string Email3 { get; set; }
+        public string LinkedIn3 { get; set; }
+        public string Twitter3 { get; set; }
+        public string TeamName { get; set; }
+    }
+}
